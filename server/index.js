990fca70938637d359295dac17402827c4c3e9a8
Shashank  Pandey <skppandey2017@gmail.com>
2021-07-10 21:37:15 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import config from 'config'
+const db = config.get('CONNECTION_URL')
+
+const app = express()
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors())
+
+const PORT = process.env.PORT || 5000
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() =>
+        app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(console.message))
+mongoose.set('useFindAndModify', false)
