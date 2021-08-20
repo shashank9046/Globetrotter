30e56e04c56059bff13d7c9909953a4b191328c9
Shashank  Pandey <skppandey2017@gmail.com>
2021-08-20 23:26:27 +0530

    backend jwt auth

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import config from 'config'
 const db = config.get('CONNECTION_URL')
 
 import postRoutes from './routes/posts.js';
-
+import userRouter from "./routes/user.js";
 
 const app = express()
 
@@ -17,6 +17,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 const PORT = process.env.PORT || 5000
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() =>
