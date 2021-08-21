f8ef8740a1841bca0172c0f0eff82a22fd5119bd
Shashank  Pandey <skppandey2017@gmail.com>
2021-08-22 01:19:12 +0530

    make it better

--- server/index.js
+++ server/index.js
@@ -18,6 +18,10 @@ app.use(cors())
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
+
+app.get('/', (req, res) => {
+    res.send("app is running");
+});
 const PORT = process.env.PORT || 5000
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() =>
